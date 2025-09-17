d457e6237e221f4344019f0707dd8b2d6db9b571
Andrew Bird <ajb@spheresystems.co.uk>
2025-09-17 23:02:33 +0100

    Build: Install meson from backports

--- raspios/build.sh
+++ raspios/build.sh
@@ -4,3 +4,8 @@ uname -a
 id -a
 apt install -y -f file sudo build-essential devscripts debhelper git
 file /bin/ls
+
+# Force meson from backports
+apt install -y wget
+wget https://ftp.debian.org/debian/pool/main/m/meson/meson_1.7.0-1_all.deb
+apt install -y -f $(pwd)/meson_1.7.0-1_all.deb
