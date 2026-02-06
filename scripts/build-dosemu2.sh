8d5ed33cf683ca2c8b0d91decbb64c4c08cc004f
Andrew Bird <ajb@spheresystems.co.uk>
2026-02-06 17:58:12 +0000

    Enable build of armhf for 32 bit RaspiOS
    Note: FDPP has an 8k DOS memory penalty for using ld.lld,
       so only use it for armhf.

--- scripts/build-dosemu2.sh
+++ scripts/build-dosemu2.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+TARGET_DISTRO="$1"
+TARGET_ARCH="$2"
+
 set -e
 
 git clone https://github.com/dosemu2/dosemu2.git dosemu2.git
 (
   cd dosemu2.git
+  if [ "$TARGET_ARCH" = "armhf" ] ; then
+    sed -i -e 's/binutils-i686-linux-gnu/llvm, lld/g' debian/control
+  fi
   mk-build-deps --install --root-cmd sudo --tool "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y"
   make deb
 )
--- scripts/build-fdpp.sh
+++ scripts/build-fdpp.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+TARGET_DISTRO="$1"
+TARGET_ARCH="$2"
+
 set -e
 
 git clone https://github.com/dosemu2/fdpp.git fdpp.git
 (
   cd fdpp.git
+  if [ "$TARGET_ARCH" = "armhf" ] ; then
+    sed -i -e 's/binutils-x86-64-linux-gnu/lld, llvm/g'\
+           -e 's/nasm-segelf/nasm/g' debian/control
+    sed -i -e 's/\bdh_auto_build\b/CROSS_LD=ld.lld USE_LTO=0 &/g' debian/rules
+  fi
   mk-build-deps --install --root-cmd sudo --tool "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y"
   ./build_deb.sh
 )
